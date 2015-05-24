f5c2c99536ce41540c41c78644a8b982cf0096f9
lonewolf <nikhilkalige@gmail.com>
2015-05-23 18:55:39 -0700

    fix jshint errors

--- server/serial/main.js
+++ server/serial/main.js
@@ -1,10 +1,10 @@
-var SerialPort = require("serialport");
+var SerialPort = require('serialport');
 
 var port;
 
 module.exports = {
     list: function(socket) {
-        socket.on("serialport", function() {
+        socket.on('serialport', function() {
             SerialPort.list(function(err, ports) {
                 var list = [];
                 if(err)
@@ -12,35 +12,36 @@ module.exports = {
                 if(ports.length > 0) {
                     ports.forEach(function(port) {
                         list.push(port.comName);
-                    })
-                    socket.emit("serialport", list);
+                    });
+                    socket.emit('serialport', list);
                 }
             });
         });
     },
 
     connect: function(socket) {
-        socket.on("serialportset", function(data) {
+        socket.on('serialportset', function(data) {
             console.log(data);
             port = new SerialPort.SerialPort(data.Ports, {
                 baudRate: data.Baud,
-                parser: SerialPort.parsers.readline("\n")
+                parser: SerialPort.parsers.readline('\n')
             });
-            port.on("error", function(data) {
-                socket.emit("serialporterror");
-            })
-            port.on("open", function() {
-                console.log("open");
-                port.on("data", function(data) {
-                    socket.emit("serialportdata", data);
-                })
-            })
-        })
+            port.on('error', function(data) {
+                socket.emit('serialporterror');
+            });
+            port.on('open', function() {
+                console.log('open');
+                socket.emit('serialportopen');
+                port.on('data', function(data) {
+                    socket.emit('serialportdata', data);
+                });
+            });
+        });
 
     },
 
     close: function(socket) {
-        socket.on("disconnect", function() {
+        socket.on('disconnect', function() {
             if(port && (port.hasOwnProperty('isOpen')) && port.isOpen()) {
                 port.removeAllListeners();
                 port.close();
