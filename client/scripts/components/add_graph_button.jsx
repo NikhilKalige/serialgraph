b6e6b5c4eb0de60562e97f00cd470f63ca0ef4dc
lonewolf <nikhilkalige@gmail.com>
2015-05-28 00:38:07 -0700

    Add graph button

---
+++ client/scripts/components/add_graph_button.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var GraphFormActionCreators = require('../actions/graphFormActionCreators');
+
+module.exports = React.createClass({
+    onClick: function() {
+        GraphFormActionCreators.for(this).addGraphForm();
+    },
+
+    render: function() {
+        return (
+            <div className='container'>
+                <button class="btn btn-primary btn-raised" onClick={this.onClick}>Add Graph</button>
+            </div>
+        );
+    }
+});
